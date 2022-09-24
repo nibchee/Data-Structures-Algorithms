953909459e33489a6a377e0c890d27a5a8e28b82
nibchee <nirbhaybansal28@gmail.com>
2022-09-24 15:28:13 +0530

    Array Demo

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //declare
+        int []arr=null;
+
+        System.out.println(arr);
+
+        //allocate space
+        arr=new int[5];
+        System.out.println(arr);
+    }
+
+}
